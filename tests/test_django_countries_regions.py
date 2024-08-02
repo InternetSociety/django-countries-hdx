9d37a0fbff096798f92909044cdbf66aaddf48da
Garrett Coakley <garrettc@gmail.com>
2024-08-02 16:12:29 +0100

    More tests

--- tests/test_django_countries_regions.py
+++ tests/test_django_countries_regions.py
@@ -1,27 +1,34 @@
 from unittest import TestCase
 from django_countries_regions import regions
+from django_countries.fields import Country
+from django.conf import settings
 
+settings.configure()
+
+
+# Test data
 world_regions = ["AQ", "BV", "IO", "CX", "CC", "TF", "HM", "GS", "UM"]
 au_nz_subregions = ["AU", "NZ", "NF"]
 
 
 class TestCountry(TestCase):
-    # TODO: How to test methods that extend an external app?
     def test_country_region(self):
-        return True
+        query = Country("AF").region
+        self.assertEqual(query, "XD")
 
     def test_country_subregion(self):
-        return True
+        query = Country("AF").subregion
+        self.assertEqual(query, "XT")
 
 
 class TestRegions(TestCase):
     def test_countries_by_region(self):
         query = regions.countries_by_region("XA")
-        self.assertCountEqual(world_regions, query)
+        self.assertCountEqual(query, world_regions)
 
     def test_countries_by_subregion(self):
         query = regions.countries_by_subregion("QP")
-        self.assertCountEqual(au_nz_subregions, query)
+        self.assertCountEqual(query, au_nz_subregions)
 
     def test_region_name(self):
         query = regions.region_name("XA")
